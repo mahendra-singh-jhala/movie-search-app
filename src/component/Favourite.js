ad1839064d2a865f917e653eeba81ffa2e4ea637
mahendra-singh-jhala <mahendrasinghjhala444@gmail.com>
2024-11-13 11:24:37 +0530

    first commit

--- src/component/Favourite.js
+++ src/component/Favourite.js
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 const Favourite = ({ favourite, removeFromFavourite }) => {
     return (
         <>
@@ -7,15 +9,17 @@ const Favourite = ({ favourite, removeFromFavourite }) => {
                 ) : (
                     <div className="flex flex-wrap gap-10">
                         {favourite.map(movie => (
-                            <div className="mb-8 w-80 text-center transform transition duration-500 hover:scale-90 rounded-md overflow-hidden" key={movie.imdbID}>
-                                <img src={movie.Poster} alt={movie.Title} className="w-full h-96" />
-                                <div className="bg-slate-500 py-4">
-                                    <h1 className="text-2xl">{movie.Title}</h1>
-                                    <h3 className="text-2xl">{movie.Year}</h3>
-                                    <p className="text-xl">{movie.Type}</p>
-                                    <button onClick={() => removeFromFavourite(movie.imdbID)} className="text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-4 mt-4"> Remove </button>
+                            <Link key={movie.imdbID} to={`/movie/${movie.imdbID}`}>
+                                <div className="mb-8 w-80 text-center transform transition duration-500 hover:scale-90 rounded-md overflow-hidden" key={movie.imdbID}>
+                                    <img src={movie.Poster} alt={movie.Title} className="w-full h-96" />
+                                    <div className="bg-slate-500 py-4">
+                                        <h1 className="text-2xl">{movie.Title}</h1>
+                                        <h3 className="text-2xl">{movie.Year}</h3>
+                                        <p className="text-xl">{movie.Type}</p>
+                                        <button onClick={() => removeFromFavourite(movie.imdbID)} className="text-white bg-gradient-to-r from-red-500 via-red-600 to-red-700 hover:bg-gradient-to-br font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-4 mt-4"> Remove </button>
+                                    </div>
                                 </div>
-                            </div>
+                            </Link>
                         ))}
 
                     </div>
